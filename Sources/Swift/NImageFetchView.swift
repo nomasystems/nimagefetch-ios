f792506f23b62c46114dab930ad58ad5afdd09f8
Jorge García Mateos <jorgegmateos@gmail.com>
2025-07-03 09:43:22 +0200

    feat: add async setImage (#30)
    Refs: #ECOREIOS-2082, #STRADIOS-11703

--- Sources/Swift/NImageFetchView.swift
+++ Sources/Swift/NImageFetchView.swift
@@ -18,4 +18,26 @@ extension NImageFetchView {
         self.__setImageFrom(urlRequest, animated: animated, completion: completion)
     }
     
+    // MARK: - Async
+    
+    @available(iOS 13.0.0, *)
+    public func setImage(
+        _ request: ImageFetchRequest,
+        animated: NImageFetchViewAnimated = .ifAsync,
+        fallbackImage: UIImage? = nil
+    ) async throws {
+        try await withCheckedThrowingContinuation { (continuation: CheckedContinuation<Void, Error>) in
+            self.__setImageFrom(
+                request,
+                animated: animated,
+                fallbackImage: fallbackImage
+            ) { error in
+                if let error {
+                    continuation.resume(throwing: error)
+                } else {
+                    continuation.resume()
+                }
+            }
+        }
+    }
 }
